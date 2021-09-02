f1bebc083b2d8b550c9f6ba9668138831a4468e8
rajashekhar <rajashekharpasikanti@gmail.com>
2021-09-02 14:13:02 +0530

    Check Equality of Lines

--- LineCalculation.java
+++ LineCalculation.java
@@ -15,8 +15,29 @@ public class LineCalculation {
 		double y1 = scan.nextDouble();
 		System.out.println("enter the value of y2");
 		double y2 = scan.nextDouble();
+		
+		
+		System.out.println("enter the value of x1");
+		double x11 = scan.nextDouble();
+		System.out.println("enter the value of x2");
+		double x22 = scan.nextDouble();
+		System.out.println("enter the value of y1");
+		double y11 = scan.nextDouble();
+		System.out.println("enter the value of y2");
+		double y22 = scan.nextDouble();
+		
+		
+		
 		double distance1 = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
 		System.out.println("line 1 Length :" +  distance1);
+		double distance2 = Math.sqrt((y22 - y11) * (y22 - y11) + (x22 - x11) * (x22 - x11));
+		System.out.println("line 1 Length :" +  distance2);
+		if(distance1==distance2) {
+			System.out.println("both lines are equal");
+			
+		}else {
+			System.out.println("two lines are not equal");
+		}
 
 	}
 
